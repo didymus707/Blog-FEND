3dd1d0b56aac4342a3c4d547713a4e72f1fe3189
Adewale Orotayo <didymus7007@gmail.com>
2021-04-15 11:27:00 +0100

    added redux actions and action types

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [hello, setHello] = useState(null);
+  useEffect(() =>
+    axios.get('/hello')
+      .then(res => setHello(res.data))
+      .catch(err => console.log(err))
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { hello ? <div>{ hello}</div> : null }
     </div>
   );
 }
---
+++ src/store/actions/action.js
@@ -0,0 +1,97 @@
+import {
+  SUCCESS,
+  FAILURE,
+  USER_INPUT,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  ADD_PROFILE,
+  REMOVE_PROFILE,
+  SET_DB_PROFILE,
+  REMOVE_DB_PROFILE,
+  FETCH_DB_POSTS,
+  REMOVE_DB_POSTS,
+  FETCH_POSTS_COMMENTS,
+  REMOVE_POSTS_COMMENTS
+} from './action_types';
+
+export const success = () => {
+  return {
+    type: SUCCESS
+  }
+}
+
+export const failure = () => {
+  return {
+    type: FAILURE
+  }
+}
+
+export const user_input = () => {
+  return {
+    type: USER_INPUT
+  }
+}
+
+export const login_success = () => {
+  return {
+    type: LOGIN_SUCCESS
+  }
+}
+
+export const login_failure = () => {
+  return {
+    type: LOGIN_FAILURE
+  }
+}
+
+export const add_profile = profile => {
+  return {
+    type: ADD_PROFILE,
+    payload: profile
+  }
+}
+
+export const remove_profile = () => {
+  return {
+    type: REMOVE_PROFILE
+  }
+}
+
+export const set_db_profile = profile => {
+  return {
+    type: SET_DB_PROFILE,
+    payload: profile
+  }
+}
+
+export const remove_db_profile = () => {
+  return {
+    type: REMOVE_DB_PROFILE,
+  }
+}
+
+export const fetch_db_posts = posts => {
+  return {
+    type: FETCH_DB_POSTS,
+    payload: posts
+  }
+}
+
+export const remove_db_posts = () => {
+  return {
+    type: REMOVE_DB_POSTS,
+  }
+}
+
+export const fetch_posts_commentts = comments => {
+  return {
+    type: FETCH_POSTS_COMMENTS,
+    payload: comments
+  }
+}
+
+export const remove_posts_comments = () => {
+  return {
+    type: REMOVE_POSTS_COMMENTS
+  }
+}
